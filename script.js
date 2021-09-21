4a792e3e1b0d70f6b3bba708a8941dd5944addb7
Laura Hofer <ljhofer@gmail.com>
2021-09-21 16:13:42 -0500

    Added pseudocoding and universal variables to script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+// staring page with click button to start
+    //will need:
+        //countdown in corner
+        //welcome statement
+        //quiz description
+        //start buttom
+//question called on each page
+    //populate question
+    //give options as buttons? or bulletpoints?
+    //submit button
+
+    //also needs:
+        //check if correct -- deduct 10 sec if wrong
+
+
+var questionOne = {text:"Question 1: Which type of HTML tag to we use to connect a JavaScript file to our HTML?", option1 = "header", option2 = "div", option3 = "script", option4 = "link", correctAnswer = ""}
+var questionTwo = {text: "Question 2: Which type of loop runs code until a set condition changes?", option1 = "while loop", option2 = "interval function", option3 = "for loop", option4 = "query selector", correctAnswer = ""}
+var questionThree = {text: "Question 3: Which of the following is the correct way to create a function in JavaScript?", option1 = "function functionName{}", option2 = "function functionName()", option3 = "function = functionName()", option4 = "function:function name", correctAnswer = ""}
+var questionFour = {text: "Question 4: Which of the following is the correct way to start a \"for loop\"?", option1 = "for i + 0; i > 5; i++", option2 = "for {i + 0; i > 5; i++}", option3 = "for (i + 0; i > 5; i++)", option4 = "for ()", correctAnswer = ""}
+
+var quizScore = 0; 
+
+
+
+    
