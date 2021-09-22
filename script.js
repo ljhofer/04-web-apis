075501404de07d26c7335f9f0dc26cffdb80813f
Laura Hofer <ljhofer@gmail.com>
2021-09-22 15:57:24 -0500

    Added function skeleton for pulling data from questions array and created divs for welcomePage and questionArea

--- script.js
+++ script.js
@@ -6,19 +6,21 @@ var questions = [
     
     {questionText:"Question 2: Which type of loop runs code until a set condition changes?", 
     options: ["while loop", "interval function", "for loop", "query selector"], 
-    correctAnswer = "while loop"
+    correctAnswer: "while loop"
     },
 
     {questiontext:"Question 3: Which of the following is the correct way to create a function in JavaScript?", 
     options: ["function functionName{}", "function functionName()", "function = functionName()", "function:function name"],
-    correctAnswer = "function = functionName()"
+    correctAnswer: "function = functionName()"
     },
 
     {questionText:"Question 4: Which of the following is the correct way to start a \"for loop\"?", 
     options: ["for i + 0; i > 5; i++", "for {i + 0; i > 5; i++}", "for (i + 0; i > 5; i++)", "for ()"],
-    correctAnswer = "for (i + 0; i > 5; i++)"
+    correctAnswer: "for (i + 0; i > 5; i++)"
     }
 ]
+
+var currentQuestionIndex = 0
 
 // Global variables:
 var numberCorrect = 0; 
@@ -28,18 +30,20 @@ var answerThirdQuestion = ""
 var answerFourthQuestion = ""
 
 //Variables for starting page text, button, and countdown:
+var welcomePage = document.getElementById("welcomePage")
 var welcomeBanner = document.getElementById("welcomeBanner");
 var instructions = document.getElementById("instructions");
 var startButton = document.getElementById("startButton");
 var countdownClock = document.getElementById("countdownClock");
 
 // Variables for text of questions:
-// var questionText = document.getElementById("question-text");
-// var optionList = document.getElementById("possible-answers");
-// var optionOne = document.getElementById("li1");
-// var optionTwo = document.getElementById("li2");
-// var optionThree = document.getElementById("li3");
-// var optionFour = document.getElementById("li4");
+var questionArea = document.getElementById("questionArea")
+var questionText = document.getElementById("question-text");
+var optionList = document.getElementById("possible-answers");
+var optionOne = document.getElementById("li1");
+var optionTwo = document.getElementById("li2");
+var optionThree = document.getElementById("li3");
+var optionFour = document.getElementById("li4");
 
 
 var timeLeft
@@ -57,25 +61,51 @@ var timeLeft
 
 // }
 
-
-
     
 // load firstQuestion(); on click of button
 function firstQuestion() {
     //Clear original text
-    welcomeBanner.style.display = "none";
-    instructions.style.display = "none";
-    startButton.style.display = "none";
-    
-    // Make UL visible again:
-    // optionList.style.visibility = "visible";
+    welcomePage.style.display = "none";
 
-    // Loads question text:
+    // Loads question text:  
+    var currentQuestion = questions[0];
+    questionText.textContent = currentQuestion.questionText;
+    optionOne.textContent = currentQuestion.options[0];
+    optionTwo.textContent = currentQuestion.options[1];
+    optionThree.textContent = currentQuestion.options[2];
+    optionFour.textContent = currentQuestion.options[3];
+    
+        
+    //This way works:
     // questionText.textContent = "Question 1: Which type of HTML tag to we use to connect a JavaScript file to our HTML?";
     // optionOne.textContent = "header";
     // optionTwo.textContent = "div"; 
     // optionThree.textContent = "script";
     // optionFour.textContent = "link";
+       
+} 
+
+firstQuestion();
+
+function secondQuestion() {
+
+    var currentQuestion = questions[1];
+    questionText.textContent = currentQuestion.questionText;
+    optionOne.textContent = currentQuestion.options[0];
+    optionTwo.textContent = currentQuestion.options[1];
+    optionThree.textContent = currentQuestion.options[2];
+    optionFour.textContent = currentQuestion.options[3];
+
+}
+
+secondQuestion();
+
+
+
+ // Make UL visible again:
+    // optionList.style.visibility = "visible";
+
+    
 
 
     //Need to link this to user selection on click:
@@ -90,14 +120,5 @@ function firstQuestion() {
     // }
 
     // secondQuestion();
-} 
-
-firstQuestion();
-
-
-
-
-//also needs:
-        //check if correct -- deduct 10 sec if wrong
 
     
