33a1fc239c535dba336e57f085234ed0944f30d9
Laura Hofer <ljhofer@gmail.com>
2021-09-24 09:26:47 -0500

    Added display of highest score and play again link

--- assets/javascript/script2.js
+++ assets/javascript/script2.js
@@ -1,9 +1,25 @@
 //Global variables
-highestScore = document.getElementById("#highest-score");
+var highestScore = document.getElementById("highest-score");
+var playAgain = document.getElementById("play-again");
+
+currentHighestScore ="";
+currentHighestScoreInitials ="";
+
+// Checks local storage for current high score data displays results
+if (localStorage.getItem("currentHighScore") !== null) {
+    currentHighestScore = localStorage.getItem("currentHighScore");
+    currentHighestScoreInitials = localStorage.getItem("userInitials");
+
+    highestScore.textContent = currentHighestScoreInitials + " - " + currentHighestScore;
+    } else {
+    highestScore.textContent = "No high score yet."
+}
 
 
-
-
+playAgain.addEventListener("click", function(e) {
+    window.open("./index.html", "_self");
+        
+})
 
 
 
